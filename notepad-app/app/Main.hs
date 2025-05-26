e306c637f5ceb3eedb9f857c0ad6f57bceea89af
Meta77 <horokusa@keio.jp>
2025-05-26 22:48:14 +0900

    エラー解消の実験　

--- notepad-app/app/Main.hs
+++ notepad-app/app/Main.hs
@@ -2,97 +2,94 @@
 {-# LANGUAGE GADTs             #-}
 {-# LANGUAGE TypeApplications  #-}
 
--- Scottyを 'Scotty' という名前で修飾インポートします
 import qualified Web.Scotty.Trans as Scotty
 
 import Network.HTTP.Types.Status (status200, status201, status204, status404, status500)
 import Data.Aeson (object, (.=))
 import Database.Persist (Entity, Key)
--- SqlBackend をインポートリストに追加します
 import Database.Persist.Sqlite (fromSqlKey, toSqlKey, SqlBackend)
 import Control.Monad.Reader (ReaderT, runReaderT, lift)
 import Control.Monad.IO.Class (liftIO, MonadIO)
 import Data.Pool (Pool)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
--- Text, pack, unpack をインポートします
 import Data.Text (Text, pack, unpack)
--- Int64 をインポートします
 import Data.Int (Int64)
 
 import Model (Note(..), NoteInput(..), NoteId)
 import qualified DB as DB
 
--- AppM の定義 (ScottyT の型引数を修正)
+-- AppM の定義
 type AppM = ReaderT (Pool SqlBackend) IO
 
--- Scotty.ActionT を使うように修正
-runIO :: Pool SqlBackend -> ReaderT (Pool SqlBackend) IO a -> Scotty.ActionT Text IO a
-runIO pool action = liftIO $ runReaderT action pool
+-- ScottyAction の型エイリアスを削除
+
+-- DBアクションを実行し、ScottyT IO にリフトするヘルパー
+-- 戻り値の型を Scotty.ScottyT Text IO a に変更
+runDB :: Pool SqlBackend -> ReaderT (Pool SqlBackend) IO a -> Scotty.ActionT Text IO a
+runDB pool action = liftIO $ runReaderT action pool
 
 main :: IO ()
 main = do
-    pool <- DB.createDbPool
-    DB.runDbMigration pool -- ← DB.runDbMigration になっていることを確認
+    pool <- DB.createPool
+    DB.runDBMigration pool
     putStrLn "Starting server on port 3000..."
 
-    -- Scotty.scotty ではなく Scotty.scottyT を使います
-    -- また、ReaderT を実行する方法を ScottyT に渡します
-    Scotty.scotty 3000 $ do
+    Scotty.scottyT 3000 id $ do
 
-        let runAction = runIO pool -- このヘルパーはもう使わないかも...
-                        -- 直接 liftIO で実行するか、ScottyT/ReaderT を使う
-
-        -- Scotty.middleware を使います
         Scotty.middleware logStdoutDev
 
-        -- Scotty.defaultHandler を使います
         Scotty.defaultHandler $ \e -> do
             Scotty.status status500
-            Scotty.json $ object ["error" .= e] -- エラーメッセージの型を Text に
+            Scotty.json $ object ["error" .= e]
 
-        -- GET /notes: メモ一覧取得 (Scotty.get を使う)
+        -- GET /notes: メモ一覧取得
         Scotty.get "/notes" $ do
-            notes <- liftIO $ runReaderT DB.getAllNotes pool
+            notes <- runDB pool DB.getAllNotes
             Scotty.json notes
 
-        -- POST /notes: メモ作成 (Scotty.post を使う)
+        -- POST /notes: メモ作成
         Scotty.post "/notes" $ do
+            -- Scotty.ScottyT Text IO NoteInput に変更
             input <- Scotty.jsonData :: Scotty.ActionT Text IO NoteInput
-            newNote <- liftIO $ runReaderT (DB.createNote input) pool
+            newNote <- runDB pool (DB.createNote input)
             Scotty.status status201
             Scotty.json newNote
 
-        -- GET /notes/:id: メモ詳細取得 (Scotty.get を使う)
+        -- GET /notes/:id: メモ詳細取得
         Scotty.get "/notes/:id" $ do
-            noteId' <- Scotty.param "id" :: Scotty.ActionT Text IO Int64 -- Int64 を使う
+            -- Scotty.ScottyT Text IO Int64 に変更
+            noteId' <- Scotty.param "id" :: Scotty.ActionT Text IO Int64
             let noteId = toSqlKey noteId'
-            mNote <- liftIO $ runReaderT (DB.getNoteById noteId) pool
+            mNote <- runDB pool (DB.getNoteById noteId)
             case mNote of
                 Just note -> Scotty.json note
                 Nothing   -> do
                     Scotty.status status404
                     Scotty.json $ object ["error" .= ("Note not found" :: Text)]
 
-        -- PUT /notes/:id: メモ更新 (Scotty.put を使う)
+        -- PUT /notes/:id: メモ更新
         Scotty.put "/notes/:id" $ do
-            noteId' <- Scotty.param "id" :: Scotty.ActionT Text IO Int64 -- Int64 を使う
+            -- Scotty.ScottyT Text IO Int64 に変更
+            noteId' <- Scotty.param "id" :: Scotty.ActionT Text IO Int64
             let noteId = toSqlKey noteId'
+            -- Scotty.ScottyT Text IO NoteInput に変更
             input <- Scotty.jsonData :: Scotty.ActionT Text IO NoteInput
-            mUpdatedNote <- liftIO $ runReaderT (DB.updateNoteById noteId input) pool
+            mUpdatedNote <- runDB pool (DB.updateNoteById noteId input)
             case mUpdatedNote of
                 Just updatedNote -> Scotty.json updatedNote
                 Nothing          -> do
                     Scotty.status status404
                     Scotty.json $ object ["error" .= ("Note not found" :: Text)]
 
-        -- DELETE /notes/:id: メモ削除 (Scotty.delete を使う)
+        -- DELETE /notes/:id: メモ削除
         Scotty.delete "/notes/:id" $ do
-            noteId' <- Scotty.param "id" :: Scotty.ActionT Text IO Int64 -- Int64 を使う
+            -- Scotty.ScottyT Text IO Int64 に変更
+            noteId' <- Scotty.param "id" :: Scotty.ActionT Text IO Int64
             let noteId = toSqlKey noteId'
-            mNote <- liftIO $ runReaderT (DB.getNoteById noteId) pool
+            mNote <- runDB pool (DB.getNoteById noteId)
             case mNote of
                 Just _ -> do
-                    liftIO $ runReaderT (DB.deleteNoteById noteId) pool
+                    runDB pool (DB.deleteNoteById noteId)
                     Scotty.status status204
                 Nothing -> do
                     Scotty.status status404
